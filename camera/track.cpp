2409afaf0edca340853f0e5a1cd76bf4c382814d
Niraj Raniga <niraj.xr@gmail.com>
2016-07-31 19:45:47 -0700

    added contour detection

--- camera/track.cpp
+++ camera/track.cpp
@@ -1,8 +1,10 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "iostream"
 
 using namespace cv;
+using namespace std;
 
 int main( int argc, char** argv ) {
 	//Scalar greenLower = (29, 86, 6);
@@ -22,20 +24,28 @@ int main( int argc, char** argv ) {
 	for (;;) {
 		Mat frame;
 		cap >> frame; // get a new frame from camera
-		
 		cvtColor(frame, hsv, COLOR_BGR2HSV);
-		imshow("original", hsv);
-		inRange(hsv, Scalar(0,100,100), Scalar(50,255,255), mask);
 
+		imshow("original", hsv);
+		inRange(hsv, Scalar(100,80,80), Scalar(115,120,120), mask);
 		// Apply erosion or dilation on the image
 		// (in, out, kernel, anchor, iteration, bordertype, bordervalue)
-		//erode( mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue() );
-		//dilate( mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue() );
+		erode( mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue() );
+		dilate( mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue() );
 
-		//findContours( contourOutput, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE );
-
-
+		vector<vector<Point> > contours;
+		vector<Vec4i> hierarchy;
+		RNG rng(12345);
+		findContours( mask, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE, Point(0, 0) );
+		
+		Mat drawing = Mat::zeros( mask.size(), CV_8UC3 );
+		for( int i = 0; i< contours.size(); i++ ){
+		    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+		    drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+		}     
+		  
 		imshow("hsv", mask);
+		imshow("contours", drawing);
 		if (waitKey(30) >= 0) break;
 	}
 
